7150ed12abd67b1a6f6feba5fbd201792c6b8cd6
Student <nannapat.it@mail.kmutt.ac.th>
2023-08-29 16:25:40 +0700

    dark mode suuuuuuuuuuy

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,18 @@ const notes = ref('')
 const status = ref('')
 const accept = ref(false)
 const roles = ref([])
-const filterGroupWorks = computed(()=>{
-  return groupworks.value.filter((element)=>{
+const toggleDark = ref(false)
+const filterGroupWorks = computed(() => {
+  return groupworks.value.filter((element) => {
     return element.projectName.toLowerCase().includes(searchValue.value.toLowerCase())
-})
+  })
 })
 </script>
 
 <template>
   <div class="w-full min-h-screen">
     <!-- Header -->
-    <div class="w-full flex p-3">
+    <div class="w-full flex p-3" :class="{'dark-mode' : toggleDark}">
       <!-- left panel -->
       <div class="w-1/2">
         <div class="flex flex-row space-x-2">
@@ -34,6 +35,13 @@ const filterGroupWorks = computed(()=>{
           </div>
         </div>
       </div>
+      <div class="flex justify-between items-center">
+        <h3>dark mode</h3>
+        <input type="checkbox" v-model="toggleDark" class="checkbox" id="checkbox">
+        <label for="checkbox" class="checkbox-label flex justify-center items-center">
+          <span class="ball"></span>
+        </label>
+      </div>
       <!-- right panel -->
       <div class="w-1/2">
         <div class="flex space-x-3 justify-end">
@@ -45,7 +53,7 @@ const filterGroupWorks = computed(()=>{
     <!-- body -->
     <div class="w-full">
       <!-- table heading -->
-      <div class="p-5 grid grid-cols-7 text-md font-semibold">
+      <div class="p-5 grid grid-cols-7 text-md font-semibold " :class="{'dark-mode' : toggleDark}">
         <h3>Section</h3>
         <h3>Group Name</h3>
         <h3 class="col-span-3">GitHub Repository</h3>
@@ -53,7 +61,11 @@ const filterGroupWorks = computed(()=>{
       </div>
       <!-- table body -->
       <div class="p-5 grid grid-cols-7 text-xs" v-for="(group, index) in filterGroupWorks" :key="index"
-        :class="index % 2 === 0 ? 'bg-gray-100' : 'bg-white'">
+      :class="[
+    index % 2 === 0 ? 'bg-gray-100' : 'bg-white',
+    toggleDark && index % 2 === 0 ? 'dark-row' : '',
+    toggleDark && index % 2 !== 0 ? 'darker-row' : ''
+  ]">
         <p>{{ group.section }}</p>
         <p>{{ group.projectName }}</p>
         <p class="col-span-3">{{ group.githubRepo }}</p>
@@ -64,7 +76,7 @@ const filterGroupWorks = computed(()=>{
         </div>
       </div>
     </div>
-    <div class="flex flex-col gap-4 justify-center items-center ">
+    <div class="flex flex-col gap-4 justify-center items-center " :class="{'dark-mode' : toggleDark}">
       <h1 class="mt-6 self-center font-bold text-2xl ">Adding a New Groupwork</h1>
       <div class="flex justify-between">
         <h3 class="mr-3">Section: </h3>
@@ -77,37 +89,107 @@ const filterGroupWorks = computed(()=>{
       </div>
 
       <div class="flex justify-between">
-          Status:
-          <select
-            v-model="status"
-            class=" mr-3 ml-3 border border-gray-300 rounded-lg p-0.5 w-1/2"
-          >
-            <option value="leader">leader</option>
-            <option value="member">member</option>
-          </select>
-          <span>{{ status }}</span>
-        </div>
-        <div>
-          <input type="checkbox" v-model="accept" />
-          You accept a requirement of group work
-          <span>({{ accept }})</span>
-        </div>
-
-        <div class="flex space-x-2">
-          <label for="">Select your role:</label>
-
-          <input type="checkbox" value="Programming" v-model="roles" />
-          <label for="">Programming</label>
-
-          <input type="checkbox" value="UX/UI" v-model="roles" />
-          <label for="">UX/UI</label>
-
-          <input type="checkbox" value="Testing" v-model="roles" />
-          <label for=""> Testing</label>
-        </div>
-        <span>{{ roles }}</span>
+        Status:
+        <select v-model="status" class=" mr-3 ml-3 border border-gray-300 rounded-lg p-0.5 w-1/2">
+          <option value="leader">leader</option>
+          <option value="member">member</option>
+        </select>
+        <span>{{ status }}</span>
       </div>
+      <div>
+        <input type="checkbox" v-model="accept" />
+        You accept a requirement of group work
+        <span>({{ accept }})</span>
+      </div>
+
+      <div class="flex space-x-2">
+        <label for="">Select your role:</label>
+
+        <input type="checkbox" value="Programming" v-model="roles" />
+        <label for="">Programming</label>
+
+        <input type="checkbox" value="UX/UI" v-model="roles" />
+        <label for="">UX/UI</label>
+
+        <input type="checkbox" value="Testing" v-model="roles" />
+        <label for=""> Testing</label>
+      </div>
+      <span>{{ roles }}</span>
     </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.checkbox {
+  opacity: 0;
+  position: absolute;
+}
+
+.checkbox-label {
+  background-color: #111;
+  width: 50px;
+  height: 26px;
+  border-radius: 50px;
+  position: relative;
+  padding: 5px;
+  cursor: pointer;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  transition: background-color 0.2s ease-in-out; /* Add transition */
+}
+
+.checkbox-label .ball {
+  background-color: #fff;
+  width: 21px;
+  height: 21px;
+  position: absolute;
+  left: 1px;
+  right: 1px; /* Adjusted positioning */
+  border-radius: 50%;
+  transition: transform 0.2s linear, background-color 0.2s linear; ;
+}
+
+.checkbox:checked + .checkbox-label .ball {
+  transform: translateX(20px);
+  background-color: black; 
+}
+
+/* Change background color when the circle is on the left */
+.checkbox:checked + .checkbox-label {
+  background-color: white;
+}
+
+/* Set the border to black */
+.checkbox-label {
+  border: 2px solid #000;
+}
+
+.dark-mode {
+  background-color: black;
+  color: white;
+  transition: transform 0.2s linear, background-color 0.2s linear, color 0.2s linear;
+}
+
+.light-mode {
+  background-color: white;
+  color: black;
+  transition: transform 0.2s linear, background-color 0.2s linear, color 0.2s linear;
+}
+
+/* Darker background color for odd rows in dark mode */
+.dark-row {
+  background-color: #333;
+  color: white;
+  transition: transform 0.2s linear, background-color 0.2s linear, color 0.2s linear;
+}
+
+
+
+/* Lighter background color for even rows in dark mode */
+.darker-row {
+  background-color: #444;
+  color: white;
+  transition: transform 0.2s linear, background-color 0.2s linear, color 0.2s linear;
+}
+</style>
